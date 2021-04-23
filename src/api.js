f98e22be086b35292c8482efb994c6c9c6500e97
Adam Davis <adavisdeveloper@gmail.com>
2021-04-23 03:45:44 -0400

    add basic api data and datetime handler

---
+++ src/api.js
@@ -0,0 +1,30 @@
+//Base URL
+const base_url =
+  "https://api.rawg.io/api/games?key=4fa0e4c08fb54aaaaff103944c7ec89b";
+
+// Get date
+//Month
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+//Day
+const getCurrentDay = () => {
+  const day = new Date().getDay();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+//Year
+const year = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+
+// Popular Games
+const popular_games = "";
