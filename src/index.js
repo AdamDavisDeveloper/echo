dd6722194b34c2d2dad376a7d4e4302edf93eda4
Adam Davis <adavisdeveloper@gmail.com>
2021-04-27 23:32:07 -0400

    setup default games state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//Redux
+import { createStore } from "redux";
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    popular = [],
+    newGames = [],
+    upcoming = [],
+};
+
+const gamesReducer = (state=initState, action) => {
+    switch(action.type) {
+        case "FETCH_GAMES":
+            return {...state};
+        default:
+            return {...state};
+    }
+};
