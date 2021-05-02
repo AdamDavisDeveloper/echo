a343b33d2d244ae3424c7d23edc730d0bfa9ab3f
Adam Davis <adavisdeveloper@gmail.com>
2021-05-01 23:24:37 -0400

    async fetch w/ axios and dispatch w/ thunk

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+import { popularGamesURL } from "../api";
+
+//Action Creator
+export const loadGames = () => async (dispatch) => {
+  //Fetch via Axios
+  const popularData = await axios.get(popularGamesURL());
+  dispatch({
+    type: "FETCH_GAMES",
+    payload: {
+      popular: popularData,
+    },
+  });
+};
